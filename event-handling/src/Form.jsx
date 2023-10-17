54bfd9c8f78eaf03e71e51e66c305eadacedaa32
Charles2205 <cewudzie0@gmail.com>
2023-10-17 12:12:00 +0000

    Update

--- event-handling/src/Form.jsx
+++ event-handling/src/Form.jsx
@@ -4,22 +4,37 @@ import ("./Form.css")
 const Form =()=>{
     const [firstNumState,setFirstNumber] = useState(0)
     const [secondNumState, setSecondNumber] = useState(0)
+    const [sumState, setSumState] = useState(0)
+    
+   
+    const btnAdd = (e)=>{
+         setSumState(parseInt(firstNumState)+parseInt(secondNumState))
+    }
+
+    const onFirstNumberChange =(e)=>{
+        setFirstNumber(e.target.value)
+        setSumState(parseInt(e.target.value)+parseInt(secondNumState))
+      }
+      const onSecondNumberChange=(e)=>{
+        setSecondNumber(e.target.value)
+        setSumState(parseInt(e.target.value)+parseInt(firstNumState))
+     }
 return(
 <>
     
     <div className=" container justify-content-center align-items-center my-5">
         <h1>Addition</h1>
     <div className="input-group mb-3">
-            <input type="number" onChange={(e)=>setFirstNumber(e.target.value)} value={firstNumState} className="form-control" placeholder="Enter your first number" aria-label="Username" aria-describedby="basic-addon1"/>
+            <input type="number" onChange={onFirstNumberChange} value={firstNumState} className="form-control" placeholder="Enter your first number" aria-label="Username" aria-describedby="basic-addon1"/>
         </div>
 
         <div className="input-group mb-3">
-            <input type="number" onChange={(e)=>setSecondNumber(e.target.value)} value={secondNumState} className="form-control" placeholder="Enter your second number" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+            <input type="number" onChange={onSecondNumberChange} value={secondNumState} className="form-control" placeholder="Enter your second number" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
         </div>
 
-        <span>Your Result is : 0 </span>  <br /><br />
+        <span>Your Result is : <span>{sumState}</span></span>  <br /><br />
 
-        <button  className="btn btn-primary">Add Me</button>
+        <button onClick={btnAdd} className="btn btn-primary">Add Me</button>
 
     </div>
 </>
